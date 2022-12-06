d703b7e53e829eb03650c12ea487ed85ca5da987
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-06 01:59:50 +0100

    Working eslint test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Logo from '../public/logo.png';
 
 export default function Home() {
   return (
