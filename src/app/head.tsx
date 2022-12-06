3f25facbaeca30359e9796f237b43c23040e0fd7
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-06 02:53:05 +0100

    Moving everything to the src folder

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,7 @@ function Head() {
     <>
       <title></title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/public/favicon.ico" />
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import FontProvider from './fontProvider';
 
 function RootLayout({ children }: { children: React.ReactNode }) {
