131234f835aca960fdbd5086d73a7539bcf72041
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-07 00:28:09 +0100

    removing unused imports

--- src/app/(guest)/signIn/page.tsx
+++ src/app/(guest)/signIn/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useSession, useSignIn } from '../../../stores/hooks/user';
+import { useSignIn } from '../../../stores/hooks/user';
 import { useRouter } from 'next/navigation';
 
 function SignInPage() {
