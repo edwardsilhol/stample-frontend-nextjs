80ecf763e19f053b544fdc3dcab811eccacfebbb
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-07 11:27:02 +0100

    Changing react query production devtools declaration

--- src/app/providers/reactQueryProvider.tsx
+++ src/app/providers/reactQueryProvider.tsx
@@ -7,9 +7,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 export const queryClient = new QueryClient();
 
 const ReactQueryDevtoolsProduction = lazy(() =>
-  import('@tanstack/react-query-devtools/production').then((d) => ({
-    default: d.ReactQueryDevtools,
-  })),
+  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
 );
 
 interface Props {
