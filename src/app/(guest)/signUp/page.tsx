f807052b93351e0b4a5d32855d5037b439823963
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-08 20:25:50 +0100

    Added non working signUpForm

---
+++ src/app/(guest)/signUp/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import SignUpForm from '../../../components/forms/auth/SignUpForm';
+
+function SignUpPage() {
+  return <SignUpForm />;
+}
+
+export default SignUpPage;
