51b39b61b7efb8e93bb59318a4531101b3ebf6ba
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-17 00:15:20 +0100

    Adding PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+// TODO: use regulat next-pwa when they updated to nextjs 13 new app dir
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  sw: "service-worker.gen.js",
+  disable: process.env.NEXT_PUBLIC_NODE_ENV === 'development'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,4 +15,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
