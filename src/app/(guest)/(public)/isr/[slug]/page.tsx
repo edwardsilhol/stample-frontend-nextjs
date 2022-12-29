2a517b848c9f87e4e659954fb0f61b8729990130
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2022-12-30 00:59:38 +0100

    Adding ISR template and using mui ovverides

---
+++ src/app/(guest)/(public)/isr/[slug]/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Typography from '../../../../../components/muiOverrides/Typography';
+import Box from '../../../../../components/muiOverrides/Box';
+
+interface PageParams {
+  slug: string;
+}
+
+export const revalidate = 60; // revalidate interval for ISR
+export async function generateStaticParams(): Promise<PageParams[]> {
+  return [
+    {
+      slug: '1',
+    },
+    {
+      slug: '2',
+    },
+    {
+      slug: '3',
+    },
+  ];
+}
+
+function useStyles() {
+  return {
+    container: {
+      backgroundColor: 'primary.main',
+      height: '100vh',
+    },
+  };
+}
+
+async function getPageData(slug: string) {
+  return {
+    title: slug,
+  };
+}
+interface Props {
+  params: PageParams;
+}
+
+async function ISRPage({ params: { slug } }: Props) {
+  const styles = useStyles();
+  const { title } = await getPageData(slug);
+
+  return (
+    <Box sx={styles.container}>
+      <Typography variant="h1">{title}</Typography>
+    </Box>
+  );
+}
+export default ISRPage;
