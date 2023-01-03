183e01a05008c70709a37b902b2e8142546b69d4
Youssef Kadaoui Abbassi <youssef.kadaoui-abbassi@epita.fr>
2023-01-03 12:23:01 +0100

    Fixing main pages

--- src/app/(logged)/csrExample/page.tsx
+++ src/app/(logged)/csrExample/page.tsx
@@ -3,23 +3,33 @@
 import React from 'react';
 import { useLogout } from '../../../stores/hooks/user.hooks';
 import Button from '@mui/material/Button';
+import Box from '../../../components/muiOverrides/Box';
+import Typography from '../../../components/muiOverrides/Typography';
 
-function HomePage() {
+function useStyles() {
+  return {
+    container: {
+      height: '100vh',
+      my: 8,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  };
+}
+
+function CSRExample() {
+  const styles = useStyles();
   const logout = useLogout();
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button
-        variant="contained"
-        onClick={() => {
-          logout.mutate();
-        }}
-      >
+    <Box sx={styles.container}>
+      <Typography variant="h1">Logged</Typography>
+      <Button variant="contained" onClick={() => logout.mutate()}>
         Logout
       </Button>
-    </div>
+    </Box>
   );
 }
 
-export default HomePage;
+export default CSRExample;
