fc1c53be82fbf227d6861649d35a604d0b37e066
Youssef Kadaoui Abbassi <youssefabbassi4@gmail.com>
2023-11-12 19:04:51 +0100

    Added error boundary on team routes

--- src/app/(logged)/error.tsx
+++ src/app/(logged)/error.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { useEffect } from 'react';
-import Alert from '@mui/material/Alert';
+import Typography from '@mui/material/Typography';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 export default function Error({
   error,
@@ -17,13 +20,27 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <Alert severity="error">
-        <strong>Something went wrong!</strong>
-      </Alert>
-      <Button variant="contained" color="primary" onClick={() => reset()}>
-        Try again
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{ textAlign: 'center', pt: 8 }}
+    >
+      <ErrorOutlineIcon color="error" sx={{ fontSize: 60 }} />
+      <Typography variant="h1" color="textPrimary" gutterBottom>
+        404
+      </Typography>
+      <Typography variant="h5" color="textSecondary" gutterBottom>
+        {error.toString()}
+      </Typography>
+      <Button
+        component={Link}
+        variant="contained"
+        href="/"
+        sx={{ mt: 3, bgcolor: 'primary.main' }}
+      >
+        Back
       </Button>
-    </div>
+      <Button onClick={() => reset()}>Retry</Button>
+    </Container>
   );
 }
